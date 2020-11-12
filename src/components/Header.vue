5464266ae2932a63244f27cd5db50ab2c748125f
Vinicius-Muller <viniciusmul98@gmail.com>
2020-11-12 14:58:05 -0200

    Header initialized

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,22 @@
 <template>
-  <h1>header</h1>
+  
+  <header>
+
+    <nav>
+      
+      <router-link to="/" class="logo">
+      
+        <img src="" alt="Ranek">
+      
+      </router-link>
+
+
+      <router-link to="/loguin" class="btn">Vender / Loguin</router-link>
+
+    </nav>
+
+  </header>
+
 </template>
 
 <script>
