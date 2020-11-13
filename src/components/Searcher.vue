51a8b97b98dd5965d3cae9f83fb3a3986e2808ab
Vinicius-Muller <viniciusmul98@gmail.com>
2020-11-13 03:07:53 -0200

    Seacher and ProductsList components created plus some styles on the Home view

---
+++ src/components/Searcher.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: "Searcher"
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,39 @@
 <template>
+
   <div class="home">
+    <h1> 🎁 Compre ou Venda 🚚 </h1>
+
+    <Searcher />
+
+    <ProductsList />
   </div>
+
 </template>
 
 <script>
 
+import Searcher from "@/components/Seacher.vue";
+
+import ProductsList from "@/components/ProductsList.vue";
+import Searcher from '../components/Searcher.vue';
+
 
 export default {
   name: 'Home',
+  components:{
+    Searcher,
+    ProductsList
+  }
 }
 </script>
+
+<style scoped>
+
+h1 {
+  color: #87f;
+  font-size: 2rem;
+  margin-top:40px;
+  text-align: center;
+}
+
+</style>
