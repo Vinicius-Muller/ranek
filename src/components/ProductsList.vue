e0906ca15f27c7208fa24190aff759926fe0d727
Vinicius-Muller <viniciusmul98@gmail.com>
2020-11-13 04:05:58 -0200

    products list made and some fucntions also

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -1,10 +1,29 @@
 <template>
-  <div></div>
+  
+  <section class="products-container">
+
+
+  </section>
+
 </template>
 
 <script>
 export default {
-  name: "ProductsList"
+  name: "ProductsList",
+
+  data() {
+    return {
+      products: []
+    }
+  },
+
+  methods: {
+    getProducts() {
+      fetch("http://localhost:3000/product").then(response => response.json()).then(response =>{
+        this.products = response;
+      })
+    }
+  }
 }
 </script>
 
