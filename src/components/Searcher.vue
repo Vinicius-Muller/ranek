c4574c6605b03911965d5fb35def31e602e81fa5
Vinicius-Muller <viniciusmul98@gmail.com>
2020-11-15 11:36:56 -0200

    Search imput styles done

--- src/components/Searcher.vue
+++ src/components/Searcher.vue
@@ -2,9 +2,9 @@
 
   <form>
     
-    <input name="search" id="search" type="text" v-model="search" />
+    <input name="search" id="search" type="text" v-model="search" placeholder="Buscar...">
 
-    <input type="submit" id="magnifying-glass" value="Search"  @click.prevent="searchProducts"/>
+    <input type="submit" id="magnifying-glass" value="Search"  @click.prevent="searchProducts">
 
   </form>
 
@@ -29,6 +29,33 @@ export default {
 
 <style scoped>
 
+form  {
+  max-width:600px;
+  margin: 30px auto 60px auto;
+  position: relative;
+}
 
+#search {
+  width: 100%;
+  padding: 20px;
+  border: none;
+}
+
+#search:focus,
+#search:hover {
+  transform: scale(1.1);
+}
+
+#magnifying-glass {
+  width: 62px;
+  height: 62px;
+  background: url("../assets/search.svg") no-repeat center center;
+  text-indent: -150px;
+  border:none;
+  cursor:pointer;
+  position: absolute;
+  top: 0px;
+  right:0px;
+}
 
 </style>
