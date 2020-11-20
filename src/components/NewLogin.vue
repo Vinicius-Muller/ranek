e265fa5f1df97b4e6625b997d62d2874f334543c
Vinicius-Muller <viniciusmul98@gmail.com>
2020-11-20 10:53:38 -0300

    A form for new users created

--- src/components/NewLogin.vue
+++ src/components/NewLogin.vue
@@ -1,16 +1,59 @@
 <template>
 
-  <p>Crie uma nova conta</p>
-  
+  <section>
+
+    <h2>Crie Sua Conta</h2>
+
+    <transition>
+
+    <button v-if="!create" @click="create = true" class="btn">Criar Conta</button>
+
+    <UserForm v-else>
+
+      <button class="btn btn-form">Criar Usuario</button>
+
+    </UserForm>
+
+    </transition>
+  </section>
+
 </template>
 
 <script>
+
+import UserForm from "@/components/UserForm.vue";
+
 export default {
-  name: "NewLogin"
+  name: "NewLogin",
+  components: {
+    UserForm
+  },
+
+  data() {
+    return {
+      create: false
+    }
+  }
 
 }
 </script>
 
-<style>
+<style scoped>
+
+h2 {
+  text-align:center;
+  margin-top: 40px;
+  margin-bottom: 10px;
+}
+
+.btn {
+  width: 100%;
+  max-width: 300px;
+  margin-left: auto;
+  margin-right: auto;
+}
+.btn-form {
+  max-width: 100%;
+}
 
 </style>
---
+++ src/components/UserForm.vue
@@ -0,0 +1,54 @@
+<template>
+  
+  <form>
+    <label for="name">Nome</label>
+    <input id="name" name="name" type="text" v-model="name">
+
+    <label for="email">Email</label>
+    <input id="email" name="email" type="email" v-model="email">
+
+    <label for="password">Senha</label>
+    <input id="password" name="password" type="password" v-model="password">
+
+    <label for="cep">Cep</label>
+    <input id="cep" name="cep" type="text" v-model="cep">
+
+    <label for="number">Numero</label>
+    <input id="number" name="number" type="number" v-model="number">
+
+    <label for="street">Rua</label>
+    <input id="street" name="street" type="text" v-model="street">
+
+    <label for="state">Estado</label>
+    <input id="state" name="state" type="text" v-model="state">
+
+    <label for="city">Cidade</label>
+    <input id="city" name="city" type="text" v-model="city">
+
+    <div class="slot-wrapper">
+      <slot></slot>
+    </div>
+  </form>
+
+</template>
+
+<script>
+export default {
+  name: "UserForm"
+}
+</script>
+
+<style scoped>
+
+form  {
+  display: grid;
+  grid-template-columns: 80px 1fr;
+  align-items: center;
+}
+
+.slot-wrapper {
+  grid-column: 2;
+  margin-top: 10px;
+}
+
+</style>
