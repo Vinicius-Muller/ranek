9dc433abcd9b71fb3670f7371bd32655d99f36b6
Vinicius-Muller <viniciusmul98@gmail.com>
2020-12-01 09:58:18 -0300

    user products POST method working

--- src/components/AddProducts.vue
+++ src/components/AddProducts.vue
@@ -1,13 +1,65 @@
 <template>
-  <p>Add Products</p>
+  <form class="add-products">
+
+    <label for="name">Nome</label>
+    <input id="name" name="name" type="text" v-model="product.name">
+
+    <label for="price">Preço (R$)</label>
+    <input id="price" name="price" type="number" v-model="product.price">
+
+    <label for="images">Fotos</label>
+    <input id="images" name="images" type="file" ref="images">
+
+    <label for="description">Descrição</label>
+    <textarea id="description" name="description" v-model="product.description"></textarea>
+
+    <input class="btn" type="button" value="Adicionar Produto" @click.prevent="addProduct">
+
+  </form>
 </template>
 
 <script>
+
+import {api} from "@/services.js"
+
 export default {
-  name: "AddProducts"
+  name: "AddProducts",
+  data() {
+    return {
+      product: {
+        name: "",
+        price: "",
+        description: "",
+        images: []
+      }
+    }
+  },
+  methods: {
+    formatProducts() {
+      this.product.user_id = this.$store.state.user_id
+    },
+
+    addProduct() {
+      this.formatProducts();
+    api.post("/product", this.product).then(()=>{
+      this.$store.dispatch("getProducts")
+    })
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+
+.add-products {
+  display:grid;
+  grid-template-columns: 100px, 1fr;
+  align-items: center;
+  margin-bottom: 60px;
+}
+
+.btn {
+  grid-column: 2;
+}
 
 </style>
